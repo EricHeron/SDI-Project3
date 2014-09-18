39496365fa2eb77e74707817bf24334228bc3b74
Eric Heron <echeron@fullsail.edu>
2014-09-18 08:34:10 -0400

    Added global variables and a prompt

--- Heron_Eric_Assignment3.js
+++ Heron_Eric_Assignment3.js
@@ -4,5 +4,30 @@
 
 
 // Variables
+var pet1 = "cat";
+var pet2 = "dog";
+var pet3 = "bearded dragon";
+var numberOfPets = 3;
+var myPrompt;
 
+// Prompt
+var myPrompt = prompt("Would you like to hear about my pets?", "type yes or no in lower case");
+
+if (myPrompt === "yes"){
+	console.log("I have " + numberOfPets + " pets!");
+
+console.log("hi");
+
+
+
+
+
+
+
+
+
+
+} else {
+	console.log("Thanks for stopping by!");
+}
 
