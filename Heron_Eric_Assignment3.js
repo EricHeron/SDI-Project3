dea9db5b641b59959a9674340c778c4ab138735f
Eric Heron <echeron@fullsail.edu>
2014-09-18 10:51:55 -0400

    Added Array function and loop

--- Heron_Eric_Assignment3.js
+++ Heron_Eric_Assignment3.js
@@ -18,22 +18,60 @@ if (myPrompt === "yes"){
 	console.log("I have " + numberOfPets + " pets!");
 
 // Confirm
-var myConfirm = confirm("How about we start with my cat named Two?");
+var myConfirm = confirm("Let me tell you about my 3 pets.");
 
 // Confirm conditional
 if (myConfirm === true){
-	console.log("Number Two is the name of my cat. She was named after a character in the Austin Powers movies!");
-	console.log("She has one eye and a docked tail! I like to call her my pirate bobcat.")
+	console.log("I love all of my pets!");
 }
 
-// Boolean function with boolean logic
+// Boolean function with arguments and boolean logic (randomNum1 = 5 or greater and randomNum2 = 10 or greater) = true, otherwise false
+// If false, console.log will not display
 var myBoolean = function(randomNum1, randomNum2){
 	if (randomNum1 >= 5 && randomNum2 >= 10){
+
+// Return boolean (Only if both numbers are true will the console log run.)
 		console.log("We have a " + pet1 + ", a " + pet2 + " and a " + pet3 + ".");
 	}
 }
 // Calling of the boolean function
 myBoolean(10, 13);
+
+// Array Argument
+var petNames = ["Number Two", "Murphy", "Neilon"];
+
+// Array Function and for loop with 3 nested conditionals
+var myArray = function(){
+for (var i = 0; i < petNames.length; i++){
+	if (petNames[i] === "Number Two"){
+		console.log(petNames[0] + " is my " + pet1);
+	}
+	if (petNames[i] === "Murphy"){
+		console.log(petNames[1] + " is my " + pet2);
+	}
+	if (petNames[i] === "Neilon"){
+		console.log(petNames[2] + " is my " + pet3);
+	}
+}
+}
+// Calling of Array function
+myArray();
+
+// Array Property
+console.log("All " + petNames.length + " of my pets take a lot of responsibility.");
+
+// Array Method
+petNames.push("Sam");
+var petSitting = petNames.pop();
+console.log(petSitting + " is a kitten that my wife and I are watching for a friend. He is adorable.");
+
+
+
+
+
+
+
+
 
 
 
