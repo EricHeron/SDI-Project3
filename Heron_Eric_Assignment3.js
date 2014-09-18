c3e74b375ecf77b5fa9f0ce56931b607ccad3051
Eric Heron <echeron@fullsail.edu>
2014-09-18 08:58:49 -0400

    Added confirm

--- Heron_Eric_Assignment3.js
+++ Heron_Eric_Assignment3.js
@@ -11,22 +11,29 @@ var numberOfPets = 3;
 var myPrompt;
 
 // Prompt
-var myPrompt = prompt("Would you like to hear about my pets?", "type yes or no in lower case");
+var myPrompt = prompt("Would you like to hear about my pets?", "Type yes or no in lower case");
 
+// If user types yes the console log will out put, "I have 3 pets!"
 if (myPrompt === "yes"){
 	console.log("I have " + numberOfPets + " pets!");
 
-console.log("hi");
+// Confirm
+var myConfirm = confirm("How about we start with my cat named Two?");
+
+// Confirm conditional
+if (myConfirm === true){
+	console.log("Number Two is the name of my cat. She was named after a character in the Austin Powers movies!");
+	console.log("She has one eye and a docked tail! I like to call her my pirate bobcat.")
+}
+
+// Boolean function
 
 
 
 
 
 
-
-
-
-
+// This code will run if the user types no.
 } else {
 	console.log("Thanks for stopping by!");
 }
